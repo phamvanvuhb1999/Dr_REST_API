ff01574a409b7d2a4b5d5e9dcd222778e20ca9a6
phamvanvu <phamvanvuhb1999@gmail.com>
2021-05-12 23:17:16 +0700

    update Procfile, PORT to deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config(); //install dotenv
 const http = require('http');
 const app = require('./app')
 
-const port = 8000 //process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
